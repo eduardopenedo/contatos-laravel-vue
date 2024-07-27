ed01200128807c2846c8bcb0b439edac334054ca
エトゥアルト ヘネト <eduardorodriguespenedo@gmail.com>
2024-07-27 19:28:56 -0300

    Update api.php

--- contact-api/routes/api.php
+++ contact-api/routes/api.php
@@ -14,7 +14,7 @@ Route::prefix('/auth')->group(
 function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 }
 
 );
